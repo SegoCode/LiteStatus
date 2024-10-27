10d95eab11a4e34fee13a9f0c9b1b45fe90b2439
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-10-27 18:07:16 +0100

    fix Tailwind imports CSS filenames

--- code/front/astro.config.mjs
+++ code/front/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import genericBuildFilenames from 'astro-generic-build-filenames';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), genericBuildFilenames()],
 });
