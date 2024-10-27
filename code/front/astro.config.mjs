42b984d0bb703aecd41182e0b1f14d83ebf2a811
SegoCode <35817798+SegoCode@users.noreply.github.com>
2024-10-27 18:57:04 +0100

    Update astro.config.mjs

--- code/front/astro.config.mjs
+++ code/front/astro.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'entry.[hash].mjs',
-          chunkFileNames: 'chunks/chunk.[hash].mjs',
-          assetFileNames: 'assets/asset.[hash][extname]',
-        },
-      },
-    },
-  },
 });
